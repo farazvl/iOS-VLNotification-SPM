779e322819c8b89bff3c75460cdc92852a46e85f
rakesh-vl <134929448+rakesh-vl@users.noreply.github.com>
2024-01-09 21:16:16 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["VLNotificationServiceLib"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/farazvl/VLBeaconSwift.git", from: "1.0.3")
+        .package(url: "https://github.com/farazvl/VLBeaconSwift.git", branch: "beacon_fix")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
